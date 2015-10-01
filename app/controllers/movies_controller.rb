0843a4d344f8adf7db0b34cc31cb71c310cca168
tra38 <tra38@nau.edu>
2015-10-01 22:48:51 +0000

    Allow users to select format

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -69,6 +69,6 @@ class MoviesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def movie_params
-      params.require(:movie).permit(:title, :length, :release_year, :rating)
+      params.require(:movie).permit(:title, :length, :release_year, :rating, :format_id)
     end
 end
