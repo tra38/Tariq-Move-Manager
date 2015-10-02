9e82587382f19c3b9b44093ae056fd68a6cd90c3
tra38 <tra38@nau.edu>
2015-10-02 03:57:03 +0000

    Add some validations to Movie model (title, format, length, and release_year)

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,7 @@
 class Movie < ActiveRecord::Base
+    validates :title, { length: 1..50 }
+    validates :format, { presence: true }
+    validates :length, numericality: { only_integer: true, less_than: 500 }
+    validates :release_year, numericality: { only_integer: true, greater_than: 1800, less_than: 2100 }
     belongs_to :format
 end
