c59a9c5795284ee7d0d7923bbe6b761cf2a246d7
tra38 <tra38@nau.edu>
2015-10-07 18:24:23 -0500

    Add splash page to website; no image yet

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # GET /index
+  # GET /index.json
+  def index
+  end
+  
 end
